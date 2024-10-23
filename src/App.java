c8a9a56b0bb447514a93e19777d208b92d146d17
Manish Verma <manish.verma604@gmail.com>
2024-10-23 22:31:57 +0530

    Test case added for two sum problem.

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -6,6 +8,10 @@ public class App {
         int res = FindMiddleIndex.findMiddleElement(arr);
         System.out.println("FindMiddleIndex: Middle pointer: " + res);
         System.out.println("FindMiddleIndex: Middle element: " + arr[res]);
+
+
+        List<Integer> pairList = TwoSumProblem.findPairWithTargetSum(arr, 150);
+        pairList.forEach(i -> System.out.println("FindPairWithTargetSum: pairList " + i));
     }
 
 
