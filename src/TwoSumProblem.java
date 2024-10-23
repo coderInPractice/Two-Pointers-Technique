e1e228c2e056f3ad30daf2a21a0ca5a5e2a534ee
Manish Verma <manish.verma604@gmail.com>
2024-10-23 22:31:35 +0530

    Two sum problem solved.

--- src/TwoSumProblem.java
+++ src/TwoSumProblem.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Problem statement: 
+ * Given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ */
 public class TwoSumProblem {
 
-    public static int findPairWithTargetSum(int[] arr, int targetSum) {
-        return -1;
+    @SuppressWarnings("unchecked")
+    public static List<Integer> findPairWithTargetSum(int[] arr, int targetSum) {
+        int leftPointer = 0;
+        int rightPointer = arr.length - 1;
+        ArrayList<Integer> pairList = new ArrayList<>();
+
+        while(leftPointer < rightPointer) {
+            if(arr[leftPointer] + arr[rightPointer] == targetSum) {
+               pairList.add(arr[leftPointer]);
+               pairList.add(arr[rightPointer]);
+               return pairList;
+            } else if (arr[leftPointer] + arr[rightPointer] < targetSum) {
+                leftPointer++;
+            }else {
+                rightPointer--;
+            }
+            
+        }
+
+        return Collections.EMPTY_LIST;
     }
     
 }
