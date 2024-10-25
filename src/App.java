ff57cb792ced4474be1c3e16c742a17b3992535a
Manish Verma <manish.verma604@gmail.com>
2024-10-26 00:06:22 +0530

    Two sum problem is solved with better solution.

--- src/App.java
+++ src/App.java
@@ -1,17 +1,38 @@
-import java.util.List;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+
+        // int[] arr = {3,2,4};
+        // int res = FindMiddleIndex.findMiddleElement(arr);
+        // System.out.println("FindMiddleIndex: Middle pointer: " + res);
+        // System.out.println("FindMiddleIndex: Middle element: " + arr[res]);
+
+
+        // boolean pairListExist = TwoSumProblem.findPairWithTargetSumBetterSoln(arr, 6);
+        // // pairList.forEach(i -> System.out.println("FindPairWithTargetSum: pairList " + i));
+        // System.out.println("pairListExist " + pairListExist);
+
+        //int[] arr2 = {1, 1, 3, 3, 4, 5, 5, 7, 7, 8, 8};
+        //System.out.println(SingleOcurranceElement.findSingleOcurranceElementUsingBinarySearch(arr2));
+
+        // System.out.println(RemoveDuplicates.removeDuplicatesFromArray(arr2));
+
+        // int[] arr3 = {10, 20, 10, 30, 50, 10};
+        // int k = 10;
+        // RemoveGivenElement.removeOcurranceOfElement(arr3, k).forEach(i -> System.out.print(i + " "));
+
+        // int a[] = { 2, 3, 4, 10, 40 };
+        // int x = 10;
+        // System.out.println("BS " + BinarySearch.searchElement(a, x));
+
+        //FibonacciSeries.printFiboSeries(18);
 
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        int res = FindMiddleIndex.findMiddleElement(arr);
-        System.out.println("FindMiddleIndex: Middle pointer: " + res);
-        System.out.println("FindMiddleIndex: Middle element: " + arr[res]);
+        int[] res = ArrayRotation.rotateArray(arr);
+        for (int res2 : res) {
+            System.out.print(res2 + "\t");
+        }
 
-
-        List<Integer> pairList = TwoSumProblem.findPairWithTargetSum(arr, 150);
-        pairList.forEach(i -> System.out.println("FindPairWithTargetSum: pairList " + i));
     }
 
 
--- src/TwoSumProblem.java
+++ src/TwoSumProblem.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -14,6 +16,7 @@ public class TwoSumProblem {
         int leftPointer = 0;
         int rightPointer = arr.length - 1;
         ArrayList<Integer> pairList = new ArrayList<>();
+        Arrays.sort(arr);
 
         while(leftPointer < rightPointer) {
             if(arr[leftPointer] + arr[rightPointer] == targetSum) {
@@ -22,13 +25,28 @@ public class TwoSumProblem {
                return pairList;
             } else if (arr[leftPointer] + arr[rightPointer] < targetSum) {
                 leftPointer++;
-            }else {
+            } else {
                 rightPointer--;
             }
             
         }
 
         return Collections.EMPTY_LIST;
+    }
+
+    public static boolean findPairWithTargetSumBetterSoln(int[] nums, int target) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(Integer item: nums) {
+            int complement = target - item;
+            if(set.contains(complement)) {
+                return true;
+            }
+
+            set.add(item);
+        }
+
+        return false;
     }
     
 }
